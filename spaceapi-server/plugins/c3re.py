bcc5076e3ca25c2c52e70ac02b600ecfed537a69
Goldwaage <goldwaage.gla@gmx.de>
2020-05-15 21:46:38 +0200

    make topic names variable

--- spaceapi-server/plugins/c3re.py
+++ spaceapi-server/plugins/c3re.py
@@ -7,24 +7,28 @@ import os
 host = os.environ.get("MQTT_HOST")
 user = os.environ.get("MQTT_USER")
 pw = os.environ.get("MQTT_PASSWORD")
+topic_doorstatus = os.environ.get("MQTT_TOPIC_DOORSTATUS")
+topic_clubraumtemp = os.environ.get("MQTT_TOPIC_CLUBRAUM")
+topic_werkstatttemp = os.environ.get("MQTT_TOPIC_WERKSTATT")
+
 
 @plugins.template_function
 def c3re_get_doorstatus():
-    stream = os.popen('mosquitto_sub -u ' + quote(user) + ' -P ' + quote(pw) + ' -h ' + quote(host) + ' -t "c3re/hhdst" -C 1')
+    stream = os.popen('mosquitto_sub -u ' + quote(user) + ' -P ' + quote(pw) + ' -h ' + quote(host) + ' -t ' + quote(topic_doorstatus) + ' -C 1')
     output = stream.read().replace('\n','')
 
     return output == "1"
 
 @plugins.template_function
 def c3re_get_clubraum_temp():
-    stream = os.popen('mosquitto_sub -u ' + quote(user) + ' -P ' + quote(pw) + ' -h ' + quote(host) + ' -t "c3re/clubraumtemp" -C 1')
+    stream = os.popen('mosquitto_sub -u ' + quote(user) + ' -P ' + quote(pw) + ' -h ' + quote(host) + ' -t ' + quote(topic_clubraumtemp) + ' -C 1')
     output = stream.read().replace('\n','')
 
     return float(output)
 
 @plugins.template_function
 def c3re_get_werkstatt_temp():
-    stream = os.popen('mosquitto_sub -u ' + quote(user) + ' -P ' + quote(pw) + ' -h ' + quote(host) + ' -t "c3re/werkstatttemp" -C 1')
+    stream = os.popen('mosquitto_sub -u ' + quote(user) + ' -P ' + quote(pw) + ' -h ' + quote(host) + ' -t ' + quote(topic_werkstatttemp) + ' -C 1')
     output = stream.read().replace('\n','')
 
     return float(output)
